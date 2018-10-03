53616b8cb76343126e05dedd978358654f377d5a
kristinadarroch <dar15003@byui.edu>
2018-10-03 11:38:52 -0600

    lifecycle hooks

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -12,24 +12,25 @@ export class CockpitComponent implements OnInit {
   // you can use an alias (bpCreated)
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent});
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value});
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName, 
-      serverContent: this.newServerContent});
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value});
   }
   }
 
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,80 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,
+         OnInit,
+         Input, 
+         ViewEncapsulation, 
+         OnChanges, 
+         SimpleChanges,
+         DoCheck,
+         AfterContentInit,
+         AfterContentChecked,
+         AfterViewInit,
+         AfterViewChecked,
+         OnDestroy,
+         ViewChild,
+         ElementRef,
+         ContentChild
+       } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // you can apply styles globally
+  encapsulation: ViewEncapsulation.Emulated //None, Native
+
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   // from parent to child
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name: string;
+  // must add alias to ViewChild and type
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  constructor() { 
+    console.log('constructor called!!');
+   }
+
+   ngOnChanges(changes: SimpleChanges) {
+     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+     //Add '${implements OnChanges}' to the class.
+     console.log('ngOnChangesCalled!');
+     console.log(changes);
+   }
 
   ngOnInit() {
+    console.log('ngoninit called!');
+    // the elements in your DOM haven't been rendered yet
+    console.log('text content:' + this.header.nativeElement.textContent);
+    console.log('text content of paragraph' + this.paragraph.nativeElement.textContent);
+    
+    
+  }
+  
+  ngDoCheck(){
+    console.log('ngDoCheck called!');
   }
 
+  ngAfterContentInit(){
+    // ngContent
+    console.log('ngAfterContentInit called!');
+    console.log('text content of paragraph' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+  }
+ 
+  ngOnDestroy(){
+    console.log('ngOnDestroy called!');
+    
+  }
 }
